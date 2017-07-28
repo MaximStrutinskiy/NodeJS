c924db007dc92893ba91d486b6820cb155bc8fc6
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-07-28 13:36:00 +0300

    Add configuration.js customize server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var config = require('./config');
 
 var app = express();
 
-app.listen(3000, function (err) {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.listen(config.port, function (err) {
     if(err){
         console.log(err);
     }else{
-        console.log("Listening on port 3000");
+        console.log("Listening on port " + config.port);
     }
 });
