37a6db285b46d0d813e40c4889d677f4bab3f7bc
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-10-03 22:50:40 +0300

    Use json variables in js file.

--- user.js
+++ user.js
@@ -1,9 +1,11 @@
+var phrases = require('./ru.json');
+
 function User(name) {
     this.name = name;
 }
 
 User.prototype.hello = function(who, asd){
-    console.log("Hello, " + who.name + "Age = " + asd);
+    console.log(phrases.Hello +", " + who.name + "Age = " + asd);
 };
 
 console.log("userjs is required!");
