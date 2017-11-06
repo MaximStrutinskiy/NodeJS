614c0cf6af4d1e5983fca68809a6995c89fbee40
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-11-06 17:53:48 +0200

    Add rorters and forms

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var passport = require('passport');
 var expressValidator = require('express-validator');
 var LocalStrategy = require('passport-local').Strategy;
 var multer = require('multer');
-var upload = multer({dest:'./uploads'});
+var upload = multer({dest: './uploads'});
 var flash = require('connect-flash');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
@@ -32,7 +32,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -51,17 +51,17 @@ app.use(passport.session());
 app.use(expressValidator({
     errorFormatter: function (param, msg, value, location) {
         var namespace = param.split('.'),
-          root = namespace.shift(),
-          formParam = root;
+            root = namespace.shift(),
+            formParam = root;
 
-        while(namespace.length){
-          formParam += '[' + namespace.shift() + ']';
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
         }
 
-        return{
-          param: formParam,
-          msg: msg,
-          value: value
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
         }
     }
 }));
@@ -77,21 +77,21 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/register', function (req, res, next) {
+    res.render('register', {title: 'Register'});
+});
+
+router.post('/register', upload.single('profileimage'), function (req, res, next) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+
+    if (req.file) {
+        console.log('Upload File...');
+        var profileimage = req.file.filename;
+    } else {
+        console.log('No file uploaded...');
+        var profileimage = 'noimage.jpg';
+    }
+
+    // Form Validation
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username field is required').notEmpty();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    // Check Errors
+    var errors = req.validationErrors();
+
+    if (errors) {
+        console.log('Errors');
+    } else {
+        console.log('No errors');
+    }
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'login'});
 });
 
 module.exports = router;
